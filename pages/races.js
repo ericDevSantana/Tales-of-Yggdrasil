bbf1a3078c896e29a3f715ceecae3f5725772982
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-10-01 15:17:15 -0700

    new cards

--- pages/races.js
+++ pages/races.js
@@ -60,6 +60,30 @@ export default function Races() {
                         </Card.Body>
                     </Card>
 
+                    <Card style={{ width: '20rem' }} className={styles.card}>
+                        <Card.Img variant="top" src="/druid_avatar.jpg" />
+                        <Card.Body>
+                            <Card.Title>Druids</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+
+                    <Card style={{ width: '20rem' }} className={styles.card}>
+                        <Card.Img variant="top" src="/druid_avatar.jpg" />
+                        <Card.Body>
+                            <Card.Title>Druids</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+
                 </div>
             </main>
             
