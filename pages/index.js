562f67a40ef4ee8bd4a6cf528e9144c2f0939720
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-10-03 14:10:25 -0700

    style changes

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import {Image} from 'react-bootstrap'
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.home}>
       <Head>
         <title>Tales of Yggdrasil</title>
         <link rel="icon" href="/favicon.ico" />
@@ -14,12 +14,25 @@ export default function Home() {
         {/* <h1>Bem Vindo a</h1> */}
 
         <Image className={styles.logo} src='/extendedlogo.png' fluid/>
-        
-        <p>Tales of Yggdrasil é um RPG de mesa ainda em desenvolvimento, mas seu early access (acesso antecipado) já está disponível para ser jogado. O Livro do Jogador e Livro do Mestre podem ser adquiridos através deste site.
-        Por ainda estar no early access, o conteúdo disponível ainda é simples, mas novos conteúdos serão adicionados com o tempo, e o que está disponível poderá sofrer alterações. Quem adquirir os livros durante o período de acesso antecipado receberá as atualizações de ambos os livros gratuitamente.
-        Explore um pouco deste mundo incrível através deste site!</p>
+      
       </main>
 
+      <div style={{ backgroundColor: 'white', fontSize: '1.15rem', textAlign: 'justify', padding: '2% 15%', opacity: '100%' }}>
+        <img src="/mage_avatar.jpg" style={{ width: '150px', height: '150px', borderRadius: '50%', marginLeft: 'auto', marginRight: 'auto', display: 'block', marginTop: '10px', marginBottom: '30px'}} />
+        <p>
+            Tales of Yggdrasil é um RPG de mesa ainda em desenvolvimento, mas seu early access 
+            (acesso antecipado) já está disponível para ser jogado. O Livro do Jogador e Livro do Mestre podem ser adquiridos através deste site.
+            Por ainda estar no early access, o conteúdo disponível ainda é simples, mas novos conteúdos serão adicionados 
+            com o tempo, e o que está disponível poderá sofrer alterações. Quem adquirir os livros durante o período de acesso 
+            antecipado receberá as atualizações de ambos os livros gratuitamente.
+          </p>
+
+        <p>
+          <h3>Explore um pouco deste mundo incrível através deste site!</h3>
+        </p>
+
+      </div>
+      
     </div>
   )
 }
