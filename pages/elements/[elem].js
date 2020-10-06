06b407620599649ea2010716c57a90172b27faed
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-10-06 16:09:12 -0700

    reading txt content for each element

--- pages/elements/[elem].js
+++ pages/elements/[elem].js
@@ -1,10 +1,12 @@
 import Head from 'next/head';
 import styles from '../../styles/Home.module.css';
+import { getAllElementsIds, getText} from '../../lib/elements'
 
-export default function Element() {
+export default function Element(pageProps) {
 
-    const url = window.location.href.split('/');
-    const element = url[(url.length - 1)];
+    // const url = window.location.href.split('/');
+    const element = pageProps.data.elem;
+    console.log(pageProps.data)
 
     return (
         <div className={styles.main}>
@@ -13,7 +15,25 @@ export default function Element() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <img className={styles.element} src={'/'+`${element}`+'.png'}/>
-            <p>This is the description of the element {element}!</p>
+            <p>{pageProps.data.description}</p>
         </div>
     )
+}
+
+export async function getStaticPaths() {
+    const paths = getAllElementsIds()
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({ params }) {
+    const data = await getText(params.elem);
+
+    return {
+        props: {
+            data
+        }
+    }
 }
