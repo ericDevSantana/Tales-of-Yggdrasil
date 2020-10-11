99c8a0581a45cb7986ee092eeea233a33ada6d8e
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-10-10 23:59:16 -0700

    social media icons

--- pages/Partials/Footer.js
+++ pages/Partials/Footer.js
@@ -1,10 +1,20 @@
 import styles from '../../styles/Home.module.css'
 import CopyrightIcon from '@material-ui/icons/Copyright';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
 
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <CopyrightIcon fontSize="small" /> &nbsp; Copyright 2020. Todos os direitos Reservados.
+            <div className={styles.grid}>
+                <FacebookIcon fontSize="small" />
+                <InstagramIcon fontSize="small" />
+                Something
+            </div>
+            <div>
+                <CopyrightIcon fontSize="small" /> &nbsp; Copyright 2020. Todos os direitos Reservados.
+            </div>
+            
         </footer>
     )
 }
