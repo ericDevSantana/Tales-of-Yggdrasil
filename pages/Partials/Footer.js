d533c2302e716744e621a32b1029fd8c8dc49100
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-10-11 00:32:50 -0700

    Footer social media icons

--- pages/Partials/Footer.js
+++ pages/Partials/Footer.js
@@ -6,10 +6,12 @@ import InstagramIcon from '@material-ui/icons/Instagram';
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <div className={styles.grid}>
+            <div className={styles.grid, styles.icon}>
+                
                 <FacebookIcon fontSize="small" />
+                &nbsp;&nbsp;
                 <InstagramIcon fontSize="small" />
-                Something
+            
             </div>
             <div>
                 <CopyrightIcon fontSize="small" /> &nbsp; Copyright 2020. Todos os direitos Reservados.
